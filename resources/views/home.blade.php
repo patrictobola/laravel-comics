b7dbdf315db200ceb6648b2c02ac3738a7cc45cf
patrictobola <tobola92@gmail.com>
2023-08-03 16:59:49 +0200

    imported scss from old vue project

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comics</title>
     <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
+    @vite('resources/js/app.js')
 </head>
 
 <body>
