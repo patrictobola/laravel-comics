a1723e7efaa093831a394c5c7a90484a7808cb2a
patrictobola <tobola92@gmail.com>
2023-08-04 11:16:30 +0200

    cone header_links php and imported comics.php

---
+++ config/header_links.php
@@ -0,0 +1,43 @@
+<?php
+return [
+    [
+        'section' => 'CHARACTERS',
+        'path' => '#'
+    ],
+    [
+        'section' => 'COMICS',
+        'path' => '#'
+    ],
+    [
+        'section' => 'MOVIES',
+        'path' => '#'
+    ],
+    [
+        'section' => 'TV',
+        'path' => '#'
+    ],
+    [
+        'section' => 'GAMES',
+        'path' => '#'
+    ],
+    [
+        'section' => 'COLLECTIBLES',
+        'path' => '#'
+    ],
+    [
+        'section' => 'VIDEOS',
+        'path' => '#'
+    ],
+    [
+        'section' => 'FANS',
+        'path' => '#'
+    ],
+    [
+        'section' => 'NEWS',
+        'path' => '#'
+    ],
+    [
+        'section' => 'SHOP',
+        'path' => '#'
+    ]
+];
