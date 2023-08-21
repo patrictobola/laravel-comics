3850b6aa4a18bfb16c3cc8dec1fa3f1d84104229
patrictobola <tobola92@gmail.com>
2023-08-21 16:50:53 +0200

    minor fixes

--- resources/views/includes/comicbook_section/comicbook_info.blade.php
+++ resources/views/includes/comicbook_section/comicbook_info.blade.php
@@ -4,9 +4,11 @@
         <div class="tag">
             <figure><img src="{{ $book['thumb']}}" alt="{{ $book['title']}}"></figure>
         </div>
+        {{-- product description --}}
         <div class="wrapper">
             <div class="product-container">
                 <h1>{{ $book['title']}}</h1>
+                {{-- Availabilty section --}}
                 <div class="availability">
                     <div class="status">
                         <span>U.S. Price: {{$book['price']}}</span>
@@ -16,11 +18,20 @@
                         <a href="#">Check Availabilty</a>
                     </div>
                 </div>
+                {{-- Description  --}}
+                <p>{{ $book['description']}}</p>
             </div>
             <div class="adv">
                 <figure><img src="{{ Vite::asset('resources/images/adv.jpg')}}" alt="ADV image che se ci clicco da ispeziona funziona ma altrimenti non va. Ps. ho provate altre immagini nel path e funzionano"></figure>
             </div>
         </div>
+        {{-- product details  --}}
+        
+
+
+
+
+
             <button>LOAD MORE</button>
         </div>
     </div>
