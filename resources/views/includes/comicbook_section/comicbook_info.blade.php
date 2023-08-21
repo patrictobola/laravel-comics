854ae19cf442aa4d4ac49066d2c92a6d250f6f11
patrictobola <tobola92@gmail.com>
2023-08-21 17:39:12 +0200

    fixed colors

--- resources/views/includes/comicbook_section/comicbook_info.blade.php
+++ resources/views/includes/comicbook_section/comicbook_info.blade.php
@@ -1,4 +1,4 @@
-
+<div class="bgc">
 <div class="container comicbook-info">
         <!-- Tag  -->
         <div class="tag">
@@ -25,52 +25,56 @@
                 <figure><img src="{{ Vite::asset('resources/images/adv.jpg')}}" alt="ADV image che se ci clicco da ispeziona funziona ma altrimenti non va. Ps. ho provate altre immagini nel path e funzionano"></figure>
             </div>
         </div>
-        {{-- product details  --}}
-        
-        <div class="details">
-            <div class="talent">
-                <h2>Talent</h2>
-                <div class="art">
-                    <div class="title">Art by:</div>
-                    <div>
-                    @foreach ($book['artists'] as $writer)
-                        <a href="#">{{ $writer}}@if(!$loop->last),@endif
-                    @endforeach</a>
-                         
-                    </div>
-                </div>
-                <div class="art">
-                    <div class="title">Written by:</div>
-                    <div>
-                    @foreach ($book['writers'] as $writer)
-                        <a href="#">{{ $writer}}@if(!$loop->last),@endif
-                    @endforeach</a>
-                         
-                    </div>
-                </div>
-            </div>
-            <div class="specs">
-                <h2>Specs</h2>
-                <div class="art">
-                    <div class="title">Series:</div>
-                    <div>
-                    <a href="#">{{ $book['series']}}</a>                         
-                    </div>
-                </div>
-                <div class="art">
-                    <div class="title">U.S. Price:</div>
-                    <div>
-                    <span>{{ $book['price']}}</span>                         
-                    </div>
-                </div>
-                <div class="art">
-                    <div class="title">On Sale Date:</div>
-                    <div>
-                    <span>{{ $book['sale_date']}}</span>                         
-                    </div>
-                </div>
-            </div>
-        </div>
 
         </div>
+        {{-- product details  --}}
+        <div class="bgc-details">
+            <div class="container">
+                <div class="details">
+                    <div class="talent">
+                        <h2>Talent</h2>
+                        <div class="art">
+                            <div class="title">Art by:</div>
+                            <div>
+                            @foreach ($book['artists'] as $writer)
+                                <a href="#">{{ $writer}}@if(!$loop->last),@endif
+                            @endforeach</a>
+
+                            </div>
+                        </div>
+                        <div class="art">
+                            <div class="title">Written by:</div>
+                            <div>
+                            @foreach ($book['writers'] as $writer)
+                                <a href="#">{{ $writer}}@if(!$loop->last),@endif
+                            @endforeach</a>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="specs">
+                        <h2>Specs</h2>
+                        <div class="art">
+                            <div class="title">Series:</div>
+                            <div>
+                            <a href="#">{{ $book['series']}}</a>                         
+                            </div>
+                        </div>
+                        <div class="art">
+                            <div class="title">U.S. Price:</div>
+                            <div>
+                            <span>{{ $book['price']}}</span>                         
+                            </div>
+                        </div>
+                        <div class="art">
+                            <div class="title">On Sale Date:</div>
+                            <div>
+                            <span>{{ $book['sale_date']}}</span>                         
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
