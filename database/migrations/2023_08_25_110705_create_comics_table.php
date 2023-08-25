1de4ec29c596bcf22597fc811e184a49299bd6f6
patrictobola <tobola92@gmail.com>
2023-08-25 13:16:40 +0200

    compiled the migration

--- database/migrations/2023_08_25_110705_create_comics_table.php
+++ database/migrations/2023_08_25_110705_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price', 4, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->string('artists', 500);
+            $table->string('writers', 500);
             $table->timestamps();
         });
     }
