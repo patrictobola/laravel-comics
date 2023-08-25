8444f764425e3b8389b840b40daf2e2d7469e442
patrictobola <tobola92@gmail.com>
2023-08-25 16:44:10 +0200

    created form

--- resources/views/comicbooks/create.blade.php
+++ resources/views/comicbooks/create.blade.php
@@ -8,23 +8,62 @@
 @section('main-content')
 
     {{-- Form section  --}}
-    <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="container">
+        <form>
+            <div class="row">
+
+                @csrf
+                <div class="form-group col-6">
+                    <label for="title">Titolo fumetto</label>
+                    <input type="text" class="form-control" name="title" id="title" placeholder="Inserisci il titolo"
+                        required>
+                </div>
+                <div class="form-group col-6">
+                    <label for="price">Inserisci il prezzo</label>
+                    <input type="text" class="form-control" name="price" id="price" placeholder="es. $9.99"
+                        required>
+                </div>
+                <div class="form-group">
+                    <label for="thumb">Inserisci un URL per la copertina</label>
+                    <input type="text" class="form-control" name="thumb" id="thumb"
+                        placeholder="https://ecc..ecc..">
+                </div>
+                <div class="form-group">
+                    <label for="description">Descrivici il fumetto</label>
+                    <textarea class="form-control" id="description" name="description" rows="4"
+                        placeholder="Es. Questo fumetto parla di Boolean classe 100 in un futuro prossimo dove ogni studente viene 'promosso'. Parleremo dell'assegnazione dell'attestato di partecipazione, di quanto i nostri tutor ci hanno sopportato e della dedizione che ci hanno messo gli studenti."></textarea>
+                </div>
+                <div class="form-group col-4">
+                    <label for="series">Nome serie</label>
+                    <input type="text" class="form-control" name="series" id="series"
+                        placeholder="Inserisci la serie">
+                </div>
+                <div class="form-group col-4">
+                    <label for="sale_date">Data pubblicazione</label>
+                    <input type="date" class="form-control" name="sale_date" id="sale_date">
+                </div>
+                <div class="form-group col-4">
+                    <label for="type">Tipo fumetto</label>
+                    <select name="type" class="form-control" id="type">
+                        <option value="comic book">Comic Book</option>
+                        <option value="graphic novel">Graphic Novel</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="artists">Inserisci gli artisti separati da una virgola</label>
+                    <input type="text" class="form-control" name="artists" id="artists"
+                        placeholder="es. Pinco, Pallo, Pippo, Pluto..">
+                </div>
+                <div class="form-group">
+                    <label for="writers">Inserisci gli scrittori separati da una virgola</label>
+                    <input type="text" class="form-control" name="writers" id="writers"
+                        placeholder="es. Pinco, Pallo, Pippo, Pluto..">
+                </div>
+
+                <button type="submit" class="btn btn-primary my-5">Submit</button>
+            </div>
+        </form>
+    </div>
 
     {{-- Footer section  --}}
     @include('includes.footer.footer')
