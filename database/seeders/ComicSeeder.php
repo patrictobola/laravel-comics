48494a4a3b2c23b3ed45d891dda1bbf05bf16268
patrictobola <tobola92@gmail.com>
2023-08-25 13:21:36 +0200

    done seeder and hardcoded a test.

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,20 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $new_comic = new Comic();
+
+        $new_comic['title'] = 'test';
+        $new_comic['description'] = 'test';
+        $new_comic['thumb'] = 'test';
+        $new_comic['price'] = '20.54';
+        $new_comic['series'] = 'test';
+        $new_comic['sale_date'] = '2020-10-06';
+        $new_comic['type'] = 'test';
+        $new_comic['artists'] = 'test';
+        $new_comic['writers'] = 'test';
+
+
+        $new_comic->save();
     }
 }
