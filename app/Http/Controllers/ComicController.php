836a14d2c58b5b828230a7cc5a1b6bb3b608904e
patrictobola <tobola92@gmail.com>
2023-08-25 17:02:12 +0200

    fixed return in homepage after post a new comic

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -35,6 +35,8 @@ class ComicController extends Controller
         $new_comic = new Comic();
         $new_comic->fill($data);
         $new_comic->save();
+
+        return to_route('home');
     }
 
     /**
