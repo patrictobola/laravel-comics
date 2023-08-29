09d008558007891c1b814972b71433880d35d0da
patrictobola <tobola92@gmail.com>
2023-08-29 15:54:35 +0200

    imported and installed fontawesome trough npm

--- resources/views/includes/comicbook_section/comicbook.blade.php
+++ resources/views/includes/comicbook_section/comicbook.blade.php
@@ -21,6 +21,12 @@
             <div class="text-center d-flex justify-content-center mb-5">
                 <a class="btn btn-primary" href="{{ route('comicbooks.create') }}">Upload a new comicbook</a>
             </div>
+            <form method="POST" action="{{ route('comicbooks.destroy', $comic) }}" class="delete-controller"
+                data-name="{{ $comic->title }}">
+                @method('DELETE')
+                @csrf
+                <button class="btn btn-danger ms-2"><i class="fa-solid fa-x fa-2x"></i></button>
+            </form>
         </div>
     </div>
 </div>
